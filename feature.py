0ffd7ae79b86da51c99883a965e9d5d38f1139b4
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-01-31 23:35:13 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -1,19 +1,8 @@
 import numpy as np  # linear algebra
 import pandas as pd  # data processing
 
-import os
-import re
-import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.txt import stopwords
 
 
 def get_all_query(title, author, text):
 	sentence = title + " " + author + " " + text
-	lemmatizer = WordNetLemmatizer()
-	sentence = re.sub(r'[^\w\s]', '', sentence)
-	sentence = sentence.lower()
-	sentence = sentence.split()
-	filter_sentence = [lemmatizer.lemmatize(word) for word in sentence if not word in set(stopwords.words('english'))]
-	filter_sentence = ' '.join(filter_sentence)
-	return [filter_sentence]
+	return [sentence]
