8f3ba2826c43dfaf7c7085873fb6330e7f180c13
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-06-05 12:29:32 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -1,9 +1,4 @@
 import re
-import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-
-lemmatizer = WordNetLemmatizer()
 
 
 def get_all_query(title, author, text):
@@ -11,6 +6,5 @@ def get_all_query(title, author, text):
 	sentence = re.sub('[^a-zA-Z]', ' ', sentence)
 	sentence = sentence.lower()
 	sentence = sentence.split()
-	sentence = [lemmatizer.lemmatize(word) for word in sentence if not word in set(stopwords.words('english'))]
 	sentence = ' '.join(sentence)
 	return [sentence]
