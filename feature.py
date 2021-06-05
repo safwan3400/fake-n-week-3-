29a6baf4fc9fdd522feeb868d9b360525a049c99
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-06-05 12:41:35 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -5,6 +5,4 @@ def get_all_query(title, author, text):
 	sentence = title + " " + author + " " + text
 	sentence = re.sub('[^a-zA-Z]', ' ', sentence)
 	sentence = sentence.lower()
-	sentence = sentence.split()
-	sentence = ' '.join(sentence)
 	return [sentence]
