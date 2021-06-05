58a4b33c41cbe6e66914f28ba9b00275f395a714
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-06-05 11:28:47 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -5,6 +5,7 @@ import re
 
 def get_all_query(title, author, text):
 	sentence = title + " " + author + " " + text
-	sentence = re.sub(r'[^\w\s]', '', sentence)
+	sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+	sentence = re.sub(' +', ' ', sentence)
 	sentence = sentence.lower()
 	return [sentence]
